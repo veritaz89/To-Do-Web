a00e137853bb1d06eae40603f5a468ee5a0fe89b
Alexandra Briese <alex.briese@googlemail.com>
2017-05-16 19:45:37 +0200

    insert date from input in table

--- JS/ui.js
+++ JS/ui.js
@@ -1,3 +1,16 @@
   $( function() {
     $( "#datepicker" ).datepicker({dateFormat: "dd.mm.yy"});
   } );
+
+
+  function addRow(){
+  	var task = document.getElementById('task').value;
+  	var category = document.getElementById('category').value;
+  	var datespliter = this['datepicker'].value.split("-");
+  	var day = datespliter[2];
+  	var month = datespliter[1];
+  	var year = datespliter[0];
+  	var date = day+'.'+month+'.'+year;
+  	$("tbody").append("<tr><td>"+task+"</td><td>"+date+"</td><td>"+category+"</td></tr>");
+  	
+  };
